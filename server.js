e1289d5b6e86ef66fd27cb4090e2779565f55eb3
Farbaks <farouk.bakre@serveconsulting.com>
2021-02-01 12:23:34 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const path = require('path');
 
 const app = express();
+
 app.use(express.static('./dist/taiga-uitest'));
-app.get('*', function (req, res) {
-    res.sendFile('index.html', { root: '/dist/taiga-uitest' }
-    );
-});
+
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/taiga-uitest/'}),
+);
+
 app.listen(process.env.PORT || 8080);
